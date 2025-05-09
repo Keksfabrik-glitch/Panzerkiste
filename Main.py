a53ce6dc7fd26713cca0a422d5ea20a6c548780a
Keksfabrik-glitch <andreas.baumann.2011@mein.gmx>
2025-05-09 14:11:34 +0200

    Update Main.py

---
+++ Main.py
@@ -0,0 +1,27 @@
+# Pygame Template
+import pygame
+
+# Setup
+pygame.init()
+BREITE = 600
+HOEHE = 600
+fenster = pygame.display.set_mode((BREITE, HOEHE))
+pygame.display.set_caption("Panzerkiste")
+# clock = pygame.time.Clock()
+laeuft = True
+
+# Gameloop / Spielschleife
+while laeuft:
+    # Ereignisse
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            laeuft = False
+            
+
+    pygame.display.flip()
+
+    # clock.tick(60)  # limits FPS to 60
+
+# Ende
+pygame.quit()
+exit()
