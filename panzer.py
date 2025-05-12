1e3b72414f73afce162ed3df7dad8201c416b6e9
Mister <131907731+M0v0@users.noreply.github.com>
2025-05-12 12:45:37 +0200

    Update panzer.py

--- panzer.py
+++ panzer.py
@@ -1,12 +1,49 @@
 class Player():
-    def __init__(self):
-      self.position = [0, 0]
-      self.speed = 10
-      self.Kugeln = 5
-      self.KugelnSpeed = 20
-      self.ReloudTime = 3
-      self.SchussZeit = 0.5 #Bis zum nächstem Schuss
-      self.MienenCountdown = 7
-      self.Mienen = -1 #-1 = unendlich, 
+    def __init__(self,position):
+        self.level = 1
+        self.position = pygame.Vector2(position)
+        self.speed = 10
+        self.bullets = 5
+        self.bullet_speed = 20
+        self.bullet_type = 1  # 2 = Feuer
+        self.reload_time = 3
+        self.last_shot_time = 0
+        self.mine_countdown = 7
+        self.mines = -1  # -1 = unendlich
+        self.rotation_speed = 5
+        self.turret_rotation_speed = 8
+        self.apprallen = 2
+        self.apprall_probability = 0.75
+        self.direction = pygame.Vector2(0, 0)
+        self.angle = 0
+        self.turret_angle = 0
+        self.lives = 3
+        self.active_bullets = []
     def move_right(self):
-      self.position[0] += self.speed
+        self.direction = direction_vector.normalize()
+        self.position += self.direction * self.speed
+    
+class FeindPanzer(Level):
+    def __init__(self):
+        self.level = 1
+        self.position = pygame.Vector2(position)
+        self.speed = 10
+        self.bullets = 5
+        self.bullet_speed = 20
+        self.bullet_type = 1  # 2 = Feuer
+        self.reload_time = 3
+        self.last_shot_time = 0
+        self.mine_countdown = 7
+        self.mines = -1  # -1 = unendlich
+        self.rotation_speed = 5
+        self.turret_rotation_speed = 8
+        self.apprallen = 2
+        self.apprall_probability = 0.75
+        self.direction = pygame.Vector2(0, 0)
+        self.angle = 0
+        self.turret_angle = 0
+        self.lives = 3
+        self.active_bullets = []
+        self.behaviour = 0 #Noch nicht
+        
+
