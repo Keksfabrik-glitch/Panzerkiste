6acb976b6ab1bed4a8697319ae5117976d0420d7
Keksfabrik-glitch <andreas.baumann.2011@mein.gmx>
2025-05-12 12:58:57 +0200

    Update Startbildschirm.py

--- Startbildschirm.py
+++ Startbildschirm.py
@@ -1,7 +1,8 @@
 # Pygame Template
 import pygame
+#Variablen
 stellen = ["Singelplayer" ,"Multiplayer" , "Einstellungen" , "Beenden"]
-selected_index =  0
+selected_index = 0
 # Setup
 pygame.init()
 BREITE = 600
@@ -10,28 +11,32 @@ fenster = pygame.display.set_mode((BREITE, HOEHE))
 pygame.display.set_caption("Startbildschirm")
 # clock = pygame.time.Clock()
 laeuft = True
+#Schriftart
+font = pygame.font.SysFont("Arial", 36)  # Schriftart & Größe
 
 # Gameloop / Spielschleife
 while laeuft:
-  for i in range (0,len(stellen)):
-      if i == selected_index:
+    fenster.fill((225, 225, 225))
+    for i in range (0,len(stellen)):
+        if i == selected_index:
             color = (255, 0, 0)  # ROT für Auswahl
         else:
-            color = (255, 255, 255)  # WEISS normal
+            color = (0, 0, 0)  # SCHWARZ normal
         
-      rendered = font.render(stellen[i], True, color)
-      screen.blit(rendered, (100, 100 + i * 50))  # vertikal untereinander
+        rendered = font.render(stellen[i], True, color)
+        fenster.blit(rendered, (100, 100 + i * 50))  # vertikal untereinander
 
     pygame.display.flip()
     # Ereignisse
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             laeuft = False
+            pygame.quit()
     tastatur = pygame.key.get_pressed()
     if tastatur[pygame.K_DOWN] :
-      selected_index += 1
+        selected_index += 1
     if tastatur[pygame.K_UP] :
-      selected_index += 1
+        selected_index += 1
         
             
 
